7c801cd5fa4e65ee88af9e11861fbda98bddf68c
burchynskyi <burchynskyi@gmail.com>
2017-08-07 16:25:18 +0300

    Initial commit

---
+++ src/com/lesson1/GreetingService.java
@@ -0,0 +1,18 @@
+package com.lesson1;
+
+public class GreetingService
+{
+    public static void main(String[] args)
+    {
+        if (args.length == 1)
+        {
+            System.out.println("Hello " + args[0]);
+        }
+        else
+        {
+            System.out.println("Please enter one argument11.rthjdgtfjgkjhdfk,hk");
+        }
+
+        int a = 0;
+    }
+}
