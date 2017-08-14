7a9b759dadc6e9596cb0b8b2fc14d8e956ee96c6
burchynskyi <burchynskyi@gmail.com>
2017-08-14 10:20:28 +0300

    update

---
+++ src/com/lesson2/Demo.java
@@ -0,0 +1,43 @@
+package com.lesson2;
+
+public class Demo
+{
+    private String color;
+    private int crew;
+    private  int maxSpeed;
+
+    public void printTankInfo()
+    {
+        System.out.println();
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public int getCrew()
+    {
+        return crew;
+    }
+
+    public void setCrew(int crew)
+    {
+        this.crew = crew;
+    }
+
+    public int getMaxSpeed()
+    {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed)
+    {
+        this.maxSpeed = maxSpeed;
+    }
+}
